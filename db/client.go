6fcf6edbcaa173c46d3fed127f699a2521f224e0
Fragore <alexisfonzos_@hotmail.com>
2022-09-23 15:13:44 -0300

    Change database point

--- db/client.go
+++ db/client.go
@@ -1,13 +1,29 @@
 package db
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 //Sirve para obener el objeto para interactuar con la base de datos
 func ObtenerConexionDb() (*gorm.DB, func() error, error) {
-	dsn := "root:almacen.C12@tcp(vps-1791261-x.dattaweb.com:3306)/trackin?parseTime=True"
+	host := os.Getenv("trackin-db-host")
+	if host == "" {
+		host = "vps-1791261-x.dattaweb.com:3306"
+	}
+
+	user := os.Getenv("trackin-db-user")
+	if user == "" {
+		user = "root"
+	}
+	pass := os.Getenv("trackin-db-pass")
+	if pass == "" {
+		pass = "almacen.C12"
+	}
+
+	dsn := user + ":" + pass + "@tcp(" + host + ")/trackin?parseTime=True"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, nil, err
