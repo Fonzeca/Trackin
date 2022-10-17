672774751065a0ecd66e00e5faa3a12ae54b4f4d
Pelli <martinpelli@outlook.com>
2022-10-17 09:12:26 -0300

    CAR-234 | Usar GMT-3

--- entry/manager/dataentry_manager.go
+++ entry/manager/dataentry_manager.go
@@ -44,7 +44,7 @@ func (d *DataEntryManager) ProcessData(data json.SimplyData, db *gorm.DB) error 
 		ProtocolType: data.ProtocolType,
 		Latitud:      data.Latitude,
 		Longitud:     data.Longitude,
-		Date:         data.Date.In(argTimeZone),
+		Date:         data.Date.In(argTimeZone).Add(-time.Hour * 3),
 		Speed:        data.Speed,
 		AnalogInput1: data.AnalogInput1,
 		DeviceTemp:   data.DeviceTemp,
